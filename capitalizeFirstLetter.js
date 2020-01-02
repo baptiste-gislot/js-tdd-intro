cabb69bfc1f27a5d54d3a3de592447a971c35bb6
KobeSan <baptiste.gislot@gmail.com>
2020-01-02 17:16:45 +0100

    test passed

--- capitalizeFirstLetter.js
+++ capitalizeFirstLetter.js
@@ -2,7 +2,26 @@
 const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
-
+const capitalizeFirstLetters = (input) => {
+  let output = '';
+  if (input.length > 0) {
+    let splitted = input.split(' ');
+    for (let i = 0; i < splitted.length; i++) {
+      let capitalized = '';
+      for (let j = 0; j < splitted[i].length; j++) {
+        capitalized = splitted[i][0].toUpperCase() + splitted[i].slice(1);
+      }
+      if (i === splitted.length - 1) {
+        output += capitalized;
+      } else {
+        output += capitalized + ' ';
+      }
+    }
+    return output;
+  } else {
+    return '';
+  }
+}
 
 // Check that capitalizeFirst is a function
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
